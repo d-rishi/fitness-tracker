5ae882cd3d5f422351df4f4e4694dfe97721270b
deepakrishi <deepakrishi@hotmail.com>
2021-09-05 08:04:15 +0530

    Initial Commit

---
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -0,0 +1,35 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Exercise } from '../exercise.model';
+import {MatSort} from '@angular/material/sort';
+import { TrainingService } from '../training.service';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-past-trainings',
+  templateUrl: './past-trainings.component.html',
+  styleUrls: ['./past-trainings.component.css']
+})
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit() {
+    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue) {
+    
+    this.dataSource.filter = filterValue.value.trim().toLowerCase();
+  }
+}
